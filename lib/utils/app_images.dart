7e08b7469bac1c409ee844a4f6a1c0213b7c884e
saidelatar <@gmail.com>
2024-09-03 16:59:22 +0300

    add app images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for imagesEmail circle
+  /// assets/images/email circle.svg
+  static const String imagesEmailcircle = "assets/images/email circle.svg";
+
+  /// Assets for imagesGroup 62
+  /// assets/images/Group 62.svg
+  static const String imagesGroup62 = "assets/images/Group 62.svg";
+
+  /// Assets for imagesIcOutlineMessage
+  /// assets/images/ic_outline-message.svg
+  static const String imagesIcOutlineMessage =
+      "assets/images/ic_outline-message.svg";
+
+  /// Assets for imagesMdiTicketOutline
+  /// assets/images/mdi_ticket-outline.svg
+  static const String imagesMdiTicketOutline =
+      "assets/images/mdi_ticket-outline.svg";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+}
