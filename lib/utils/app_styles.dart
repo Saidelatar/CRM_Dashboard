f840fe7e4873b0bd88b8bc12d9320b038a473004
saidelatar <@gmail.com>
2024-09-03 16:51:11 +0300

    add app styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static TextStyle styleRegular16(context) {
+    return const TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 16,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleRegular20(context) {
+    return const TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 20,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle styleBold24(context) {
+    return const TextStyle(
+      color: Color(0xff9C5D9C),
+      fontSize: 24,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleMeduim16(context) {
+    return const TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 16,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleSemiBold16(context) {
+    return const TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 16,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMeduim13(context) {
+    return const TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 13,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle styleMeduim11(context) {
+    return const TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontSize: 11,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w500,
+    );
+  }
+}
